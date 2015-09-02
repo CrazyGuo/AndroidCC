70e3c7af29a05c375f3daa921e35eb2885f7b3ab
CrazyGuo <guohuanhuan_cool@126.com>
2015-09-02 16:55:34 +0800

    主界面添加双击退出功能
    Signed-off-by: CrazyGuo <guohuanhuan_cool@126.com>

--- src/net/oschina/app/ui/MainActivity.java
+++ src/net/oschina/app/ui/MainActivity.java
@@ -1,5 +1,10 @@
 package net.oschina.app.ui;
 
+import android.view.KeyEvent;
+
+import net.oschina.app.AppConfig;
+import net.oschina.app.AppContext;
+import net.oschina.app.AppManager;
 import net.oschina.app.R;
 import net.oschina.app.R.id;
 import net.oschina.app.R.layout;
@@ -11,28 +16,31 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+	private DoubleClickExitHelper mDoubleClickExit;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		AppManager.getAppManager().addActivity(this);
+		initView();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	//@Override
+    public void initView() {
+        mDoubleClickExit = new DoubleClickExitHelper(this);
 	}
+	/**
+     * 监听返回--是否退出程序
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            // 是否退出应用
+            if (AppContext.get(AppConfig.KEY_DOUBLE_CLICK_EXIT, true)) {
+                return mDoubleClickExit.onKeyDown(keyCode, event);
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
